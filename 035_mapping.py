c2a348a7aef5935346c81c567ae73d356ca41e78
lucbrol <lucagrr@hotmail.com>
2025-07-28 12:38:03 +0000

    Completed 035_mapping

--- 035_mapping.py
+++ 035_mapping.py
@@ -26,6 +26,12 @@ for word in words: # We go through each word
 print(words)
 print(first_letters)
 
+for i in range(len(words)): # all words in uppercase
+    words[i] = words[i].upper()
+
+print(words)
+print(first_letters)
+
 # @TASK: run this program to see what it does.
 
 # @TASK: Complete this exercise.
@@ -35,7 +41,7 @@ print("Function: add_one_hundred_to_numbers")
 
 # Return a new list of each number with 100 added
 def add_one_hundred_to_numbers(numbers):
-  pass
+  return list (num + 100 for num in numbers) # returns list of numbers
 
 check_that_these_are_equal(
   add_one_hundred_to_numbers([1, 2, 3, 4]), [101, 102, 103, 104])
