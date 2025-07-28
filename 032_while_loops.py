33b2a37336d05fcbaf97e7eed04183fba3bfce88
lucbrol <lucagrr@hotmail.com>
2025-07-28 11:50:09 +0000

    completed 032_while_loops

--- 032_while_loops.py
+++ 032_while_loops.py
@@ -49,8 +49,11 @@ print("Function: add_cats_repeatedly")
 # => ['cats', 'cats', 'cats']
 
 def add_cats_repeatedly(word_list, count):
-  # ...
-  return word_list
+  i= 0  # counter initialised
+  while i < count :  # count parameter used in while loop will add string certain number of times
+    word_list.append("cats") # use off append to add string to list
+    i= i + 1 # counter 
+  return word_list  # returns new list after appended
 
 check_that_these_are_equal(
   add_cats_repeatedly([], 3), ['cats', 'cats', 'cats'])
